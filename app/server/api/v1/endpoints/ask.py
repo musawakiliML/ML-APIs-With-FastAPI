f674050ac43211021bc7b64b64f2a823d69e95e8
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-09-22 17:06:05 +0100

    Update: added updates

--- app/server/api/v1/endpoints/ask.py
+++ app/server/api/v1/endpoints/ask.py
@@ -5,7 +5,7 @@ from fastapi.encoders import jsonable_encoder
 import io
 from PIL import Image
 
-from app.server.utils.model import model
+from app.server.utils.model import model_pipeline
 
 router = APIRouter()
 
@@ -15,7 +15,7 @@ def ask(image: UploadFile, text: str = Form(...)):
         image_content = image.file.read()
         image = Image.open(io.BytesIO(image_content))
 
-        result = model(text, image)
+        result = model_pipeline(text, image)
 
         return JSONResponse(content={"answer":result}, status_code=status.HTTP_200_OK)
     except Exception as e:
