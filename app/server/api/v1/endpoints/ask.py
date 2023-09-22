cd073a948a01e5e661b3fef8e56eb62c65ba57cb
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-09-22 10:59:15 +0100

    Update

---
+++ app/server/api/v1/endpoints/ask.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Form, UploadFile, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from typing import Annotated
+
+import io
+from PIL import Image
+
+from app.server.utils.model import model
+
+router = APIRouter()
+
+@router.post("/", response_description="Ask AI about Visual Things")
+def ask(text: Annotated[str, Form()], image: UploadFile):
+    try:
+        image_content = image.file.read()
+        image = Image.open(io.BytesIO(image_content))
+
+        result = model(text, image)
+
+        return JSONResponse(content={"answer":result}, status_code=status.HTTP_200_OK)
+    except Exception as e:
+        raise HTTPException(detail=str(e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
